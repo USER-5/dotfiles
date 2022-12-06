185bdfeeb78f10d666394741c5fbb3cf4a2e2afe
USER-5 <US3R5@hotmail.com>
2022-12-07 08:36:41 +1030

    Update keybinds.lua

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -2,4 +2,4 @@ vim.cmd("nnoremap \\ Nop")
 vim.g.mapleader = "\\"
 vim.api.nvim_set_keymap('i', '<D-v>', '<c-r>+', {})
 vim.api.nvim_set_keymap('n', '<D-v>', '"+p', {})
-vim.api.nvim_set_keymap('n', '<leader>p', "mp:%prettier %<CR>'p", { silent = true })
+vim.api.nvim_set_keymap('n', '<leader>p', "mp:%!prettierd %<CR>'p", { silent = true })
