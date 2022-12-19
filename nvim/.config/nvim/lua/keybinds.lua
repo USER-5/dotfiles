46df5ede1c69bc32eb3fe4b146135bbc19a2f2e3
USER-5 <US3R5@hotmail.com>
2022-12-19 11:34:17 +1030

    Fix highlighted move command

--- nvim/.config/nvim/lua/keybinds.lua
+++ nvim/.config/nvim/lua/keybinds.lua
@@ -9,7 +9,7 @@ vim.keymap.set('n', '<D-v>', '"+p')
 
 -- allow moving highlighted code
 vim.keymap.set("v", "J", ":m '>+1<CR>gv=gv")
-vim.keymap.set("v", "K", ":m '>-2<CR>gv=gv")
+vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 -- better J behaviour (maintain cursor location)
 vim.keymap.set("n", "J", "mzJ`z")
