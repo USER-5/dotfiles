fc67460f4c88760a9fb94802fea09967ef710479
USER5 <us3r5@hotmail.com>
2024-06-22 09:12:20 +0930

    Remove Noice double-reporting LSP progress

--- nvim/.config/nvim/lua/plugins/noice.lua
+++ nvim/.config/nvim/lua/plugins/noice.lua
@@ -27,5 +27,15 @@ return {
 				opts = { skip = true },
 			},
 		},
+		lsp = {
+			progress = {
+				enabled = false
+			},
+			override = {
+				["vim.lsp.util.convert_input_to_markdown_lines"] = true,
+				["vim.lsp.util.stylize_markdown"] = true,
+				["cmp.entry.get_documentation"] = true,
+			}
+		},
 	}
 }
