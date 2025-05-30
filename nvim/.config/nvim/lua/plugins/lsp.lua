73878c917dac6e7500fdd9bad6fa7ed2fa62b011
Patrick Gregory <patrickg@canva.com>
2025-05-30 13:57:00 +0930

    Enable Autocomplete

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -5,6 +5,15 @@ return {
 		vim.lsp.enable('lua_ls')
 		vim.lsp.enable('eslint')
 		vim.lsp.enable('pyright')
+		vim.api.nvim_create_autocmd('LspAttach', {
+			callback = function(ev)
+				local client = vim.lsp.get_client_by_id(ev.data.client_id)
+				if client:supports_method('textDocument/completion') then
+					vim.lsp.completion.enable(true, client.id, ev.buf, { autotrigger = true })
+				end
+			end,
+		})
+
 	end,
 }
 
