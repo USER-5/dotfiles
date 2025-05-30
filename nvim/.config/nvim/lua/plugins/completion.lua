d8cb08b3f7ab8bc705abd62a6d97be2acaf609cc
Patrick Gregory <patrickg@canva.com>
2025-05-30 14:26:23 +0930

    Better Autocomplete

---
+++ nvim/.config/nvim/lua/plugins/completion.lua
@@ -0,0 +1,15 @@
+return {
+	{
+		'echasnovski/mini.completion',
+		dependencies = {
+			'echanovski/mini.icons',
+			config = {}
+		},
+		version = false,
+		opts = {
+			lsp_completion = {
+				source_func = 'omnifunc'
+			}
+		}
+	}
+}
