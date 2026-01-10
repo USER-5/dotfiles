70c3b972557657efabd89eb81f5e8cdf4edd0f4d
USER5 <us3r5@hotmail.com>
2026-01-10 17:03:14 +1030

    Update nix common file

--- nix-home-manager/common.nix
+++ nix-home-manager/common.nix
@@ -2,102 +2,109 @@
 
 {
 
-  programs.direnv.enable = true;
+  programs = {
+    direnv.enable = true;
 
-  programs.eza = {
-    enable = true;
-    icons = "auto";
-  };
-
-  programs.bat = {
-    enable = true;
-  };
-
-  programs.git = {
-    enable = true;
-    delta.enable = true;
-    aliases = {
-      ci = "commit";
-      lg = "log --oneline --decorate --graph";
-      pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
-    };
-    extraConfig = {
-      push.autosetupremote = "true";
-      color.diff.new = "blue";
-      delta.plus-style = "auto darkblue";
-    };
-    lfs.enable = true;
-  };
-
-  programs.zoxide = {
-    enable = true;
-    options = [ "--cmd cd" ];
-  };
-
-  programs.starship = {
-    enable = true;
-    settings.git_status.disabled = true;
-  };
-
-  programs.bash.enable = true;
-  programs.zsh = {
-    enable = true;
-    enableCompletion = true;
-    autosuggestion.enable = true;
-    syntaxHighlighting.enable = true;
-
-    history = {
-      size = 10000;
-      path = "${config.xdg.dataHome}/.histfile";
-    };
-
-    oh-my-zsh = {
+    eza = {
       enable = true;
-      plugins = [ "git" ];
-      extraConfig = ''
-        zstyle ':completion:*' completer _expand _complete _ignored _correct _approximate
-        zstyle ':completion:*' matcher-list ""
-      '';
+      icons = "auto";
     };
 
+    bat.enable = true;
+
+    git = {
+      enable = true;
+      settings = {
+        push.autosetupremote = "true";
+        color.diff.new = "blue";
+        delta.plus-style = "auto darkblue";
+        alias = {
+          ci = "commit";
+          lg = "log --oneline --decorate --graph";
+          pl = "!git pull origin $(git rev-parse --abbrev-ref HEAD)";
+        };
+      };
+      lfs.enable = true;
+    };
+
+    delta = {
+      enable = true;
+      enableGitIntegration = true;
+    };
+
+    zoxide = {
+      enable = true;
+      options = [ "--cmd cd" ];
+    };
+
+    starship = {
+      enable = true;
+      settings.git_status.disabled = true;
+    };
+
+    bash.enable = true;
+
+    zsh = {
+      enable = true;
+      enableCompletion = true;
+      autosuggestion.enable = true;
+      syntaxHighlighting.enable = true;
+
+      history = {
+        size = 10000;
+        path = "${config.xdg.dataHome}/.histfile";
+      };
+
+      oh-my-zsh = {
+        enable = true;
+        plugins = [ "git" ];
+        extraConfig = ''
+          zstyle ':completion:*' completer _expand _complete _ignored _correct _approximate
+          zstyle ':completion:*' matcher-list ""
+        '';
+      };
+
+    };
+
+    fd.enable = true;
+
+    neovim = {
+      enable = true;
+      defaultEditor = true;
+    };
   };
 
-  programs.fd.enable = true;
+  home = {
+    sessionPath = [
+      "$HOME/.local/bin"
+      "$HOME/.cargo/bin"
+    ];
 
-  programs.neovim = {
-    enable = true;
-    defaultEditor = true;
-  };
+    sessionVariables = {
+      PNPM_HOME = "$HOME/.local/bin";
+    };
 
-  home.sessionPath = [
-    "$HOME/.local/bin"
-    "$HOME/.cargo/bin"
-  ];
+    packages = [
+      pkgs.fzf
+      pkgs.htop
+      pkgs.jq
+      pkgs.nixfmt-rfc-style
+      pkgs.vtsls
+      pkgs.vscode-langservers-extracted
+      pkgs.lua-language-server
+      pkgs.pyright
+      pkgs.ruff
+      pkgs.jdt-language-server
+      pkgs.ripgrep
+      pkgs.yq-go
+      pkgs.zellij
+    ];
 
-  home.sessionVariables = {
-    PNPM_HOME = "$HOME/.local/bin";
-  };
-
-  home.packages = [
-    pkgs.fzf
-    pkgs.htop
-    pkgs.jq
-    pkgs.nixfmt-rfc-style
-    pkgs.vtsls
-    pkgs.vscode-langservers-extracted
-    pkgs.lua-language-server
-    pkgs.pyright
-    pkgs.ruff
-    pkgs.jdt-language-server
-    pkgs.ripgrep
-    pkgs.yq-go
-    pkgs.zellij
-  ];
-
-  home.shellAliases = {
-    ls = "eza --icons";
-    ll = "eza --icons -l";
-    zellij-clean = "for SESS in $(zellij ls -s -n); do zellij delete-session \"$SESS\"; done";
+    shellAliases = {
+      ls = "eza --icons";
+      ll = "eza --icons -l";
+      zellij-clean = "for SESS in $(zellij ls -s -n); do zellij delete-session \"$SESS\"; done";
+    };
   };
 
   nix = {
