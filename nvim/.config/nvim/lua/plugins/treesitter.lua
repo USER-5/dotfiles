8d024b175da1575f9ca0fb4afdfda6ac781e2f8d
Patrick Gregory <patrickg@canva.com>
2026-02-20 14:45:44 +1030

    Update treesitter, pin to main

--- nvim/.config/nvim/lua/plugins/treesitter.lua
+++ nvim/.config/nvim/lua/plugins/treesitter.lua
@@ -1,17 +1,26 @@
 return {
 	"nvim-treesitter/nvim-treesitter",
 	build = ":TSUpdate",
+	branch = "main",
 	config = function()
-		local configs = require("nvim-treesitter.configs")
-
-		configs.setup({
-			ensure_installed = { "c", "lua", "rust", "typescript", "svelte", "css", "bash", "comment", "python", "c_sharp", "diff",
-			"dockerfile", "gitcommit", "html", "javascript", "jsdoc", "json", "markdown", "yaml" },
-			highlight = {
-				enable = true,
-				additional_vim_regex_highlighting = false,
-			}
-		})
-		vim.filetype.add({ extension = { wgsl = "wgsl" } })
+		require('nvim-treesitter').install({ "c",
+		"lua",
+		"rust",
+		"typescript",
+		"svelte",
+		"css",
+		"bash",
+		"comment",
+		"python",
+		"c_sharp",
+		"diff",
+		"dockerfile",
+		"gitcommit",
+		"html",
+		"javascript",
+		"jsdoc",
+		"json",
+		"markdown",
+		"yaml" })
 	end
 }
