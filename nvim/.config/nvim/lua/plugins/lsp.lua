cbdfa73f94c5298b61ba4e478d1558c30c23b365
Patrick Gregory <patrickg@canva.com>
2026-08-19 17:09:21 +0930

    Update to tsc

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -1,7 +1,7 @@
 return {
 	"neovim/nvim-lspconfig",
 	config = function(_, opts)
-		vim.lsp.enable('tsgo')
+		vim.lsp.enable('tsc')
 		vim.lsp.enable('lua_ls')
 		vim.lsp.enable('eslint')
 		vim.lsp.enable('pyright')
