0d4d39a53858b16b4ff5382b86ffd1e2c3e3544a
USER-5 <US3R5@hotmail.com>
2026-09-14 11:16:13 +0930

    Update install.sh

--- nix-home-manager/install.sh
+++ nix-home-manager/install.sh
@@ -1,6 +1,15 @@
 #!/usr/bin/env bash
 
-rm -rf ~/.config/home-manager
-home-manager --extra-experimental-features "nix-command flakes" init
-sed -ie 's/modules = \[ \(.*\) \];/modules = \[ \1 .\/common.nix \];/' ~/.config/home-manager/flake.nix
 cp "$(dirname $0)/common.nix" ~/.config/home-manager/common.nix
+
+echo "New file copied to ~/.config/home-manager/common.nix"
+if [ -f ~/.config/home-manager/flake.nix ]; then
+        echo "If this is your first time installing, add it as a module in flake.nix"
+else
+        echo "If this is your first time installing, merge it with home.nix"
+        echo "let common = import ./common.nix {pkgs=pkgs; config=config; };"
+        echo "in"
+        echo "lib.recursiveUpdate { # Existing file here"
+        echo "}"
+        echo "common"
+fi
